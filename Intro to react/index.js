966555900173e6b432b61de675cbc9dc1f5a6d1f
ykwizera <144004543+ykwizera@users.noreply.github.com>
2024-08-02 11:30:46 +0200

    hello react

---
+++ Intro to react/index.js
@@ -0,0 +1,5 @@
+ReactDOM.render( <p><h1>hello, react</h1>this is a paragraph from react codes</p>, document.getElementById('root'))
+ReactDOM.render( 
+<ul>Trying to create lists using react<li>one</li><li>two</li></ul>, 
+document.getElementById('hehe')
+)
