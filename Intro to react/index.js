87428eddd0452959a6542c17d6c48d4f272abe45
ykwizera <144004543+ykwizera@users.noreply.github.com>
2024-08-02 17:20:20 +0200

    adding functions

--- Intro to react/index.js
+++ Intro to react/index.js
@@ -1,5 +1,19 @@
-ReactDOM.render( <p><h1>hello, react</h1>this is a paragraph from react codes</p>, document.getElementById('root'))
+ReactDOM.render( <p><h1>hello, everyone</h1>this is a paragraph from react codes</p>, document.getElementById('root'))
 ReactDOM.render( 
 <ul>Trying to create lists using react<li>one</li><li>two</li></ul>, 
 document.getElementById('hehe')
+)
+
+function MainContent () {
+    return (
+        <h1> I am learning react</h1>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <MainContent />
+        
+    </div>,
+    document.getElementById("haha")
 )
