b5707de7944e86ce7a2e78c456afd90365786953
ykwizera <144004543+ykwizera@users.noreply.github.com>
2024-08-02 17:56:18 +0200

    changes

--- Intro to react/index.js
+++ Intro to react/index.js
@@ -17,3 +17,24 @@ ReactDOM.render(
     </div>,
     document.getElementById("haha")
 )
+
+// Create a navbar in JSX:
+//     - Use the semantic `nav` element as the parent wrapper
+//     - Have an h1 element with the brand name of your "website"
+//     - Insert an unordered list for the other nav elements
+//         - Inside the `ul`, have three `li`s for "Pricing",
+//         "About", and "Contact"
+//     - Don't worry about styling yet - it'll just be plain-looking HTML for now
+
+const navbar = (
+    <nav>
+        <h1>My Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(nav, document.getElementById('root'))
